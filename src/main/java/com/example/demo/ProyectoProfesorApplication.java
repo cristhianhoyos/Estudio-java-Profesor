991e6038eb2f4dc351f0b1f88cade4d526e51542
cristhianhoyos <cristhiandavidhoyos@gmail.com>
2021-05-16 21:17:28 -0500

    My first commit

---
+++ src/main/java/com/example/demo/ProyectoProfesorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoProfesorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoProfesorApplication.class, args);
+	}
+
+}
