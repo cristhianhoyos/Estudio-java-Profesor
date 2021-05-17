ee831e6b2214b00ab1b482d1750f86f833969f8a
cristhianhoyos <cristhiandavidhoyos@gmail.com>
2021-05-17 12:54:30 -0500

    creating the tables in the data base

--- src/main/java/com/example/demo/entity/CursoEntity.java
+++ src/main/java/com/example/demo/entity/CursoEntity.java
@@ -1,5 +1,56 @@
 package com.example.demo.entity;
 
-public class CursoEntity {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Cursos")
+public class CursoEntity implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long curso_id;
+	
+	private String nombre;
+	
+	@Column(name="profesor_id")
+	private Long profesorId;
+	
+	public CursoEntity() {
+		
+	}
+	
+	public Long getCurso_id() {
+		return curso_id;
+	}
+
+	public void setCurso_id(Long curso_id) {
+		this.curso_id = curso_id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public Long getProfesorId() {
+		return profesorId;
+	}
+
+
+	public void setProfesorId(Long profesorId) {
+		this.profesorId = profesorId;
+	}
+
+	private static final long serialVersionUID = 1L;
 }
--- src/main/java/com/example/demo/entity/ProfesoresEntity.java
+++ src/main/java/com/example/demo/entity/ProfesoresEntity.java
@@ -1,5 +1,87 @@
 package com.example.demo.entity;
 
-public class ProfesoresEntity {
+import java.io.Serializable;
+import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+@Table(name="Profesores")
+public class ProfesoresEntity implements Serializable {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column (name="nombre")
+	private String nombre;
+	
+	@Column(length = 60, unique = true)
+	private String email;
+	
+	private String password;
+	
+	@Column(length = 20000)
+	private String foto;
+	
+	@Column(name= "create_at")
+	@Temporal(TemporalType.DATE)
+	private Date createAt;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
+
+	public Date getCreateAt() {
+		return createAt;
+	}
+
+	public void setCreateAt(Date createAt) {
+		this.createAt = createAt;
+	}
+
+	private static final long serialVersionUID = 1L;
 }
